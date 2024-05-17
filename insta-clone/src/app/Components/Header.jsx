b94f7ae96fcaffd44860fffe268c322026122285
Jashan1559 <jashanjot1559@]gmail.com>
2024-05-17 10:27:50 +0530

    Uploading more files

---
+++ insta-clone/src/app/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <div className = "">
+            {/* Logo */}
+            <Link href = "/"></Link>
+
+            {/* Search input */}
+
+            {/* menu items */}
+        </div>
+    </div>
+  )
+}
